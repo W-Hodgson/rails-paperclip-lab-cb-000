6a743e599ffc49a01bccddb1277cc3b7b9ee99dc
Will Hodgson <will.hodgson@current-rms.com>
2019-11-25 15:28:49 +0000

    Automatically backed up by Learn

--- db/migrate/20191125152751_add_attachment_album_cover_to_songs.rb
+++ db/migrate/20191125152751_add_attachment_album_cover_to_songs.rb
@@ -1,4 +1,4 @@
-class AddAttachmentAlbumCoverToSongs < ActiveRecord::Migration
+class AddAttachmentAlbumCoverToSongs < ActiveRecord::Migration[5.1]
   def self.up
     change_table :songs do |t|
       t.attachment :album_cover
